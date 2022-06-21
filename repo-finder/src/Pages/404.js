3b1ce417da6d9a39f3d7926b38584d66665c2a22
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-06-21 12:10:18 -0400

    added spinner

--- repo-finder/src/Pages/404.js
+++ repo-finder/src/Pages/404.js
@@ -8,8 +8,14 @@ function FourOFour () {
       <div className="text-center hero-content">
         <div className="max-w-lg">
           <h1 className="text-8xl font-bold mb-8">You have gotten lost. </h1>
-          <p className="text-5xl mb-8"> Four O Four - Page not Found</p>
+          <p className="text-5xl  mb-8"> Four O Four - Page not Found</p>
+          <Link className="btn btn-primary btn-lg" to="/">
+            <FaHome className="mr-2" />
+            Back To Home
+          </Link>
+
         </div>
+
       </div>
     </div>
   );
