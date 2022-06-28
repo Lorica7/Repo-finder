8fe845e9216e72795e660fb23124163f55232640
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-06-27 21:06:23 -0400

    added addition context and reducer

--- repo-finder/src/App.js
+++ repo-finder/src/App.js
@@ -1,32 +1,38 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
+
 import Home from './Pages/Home';
 import About from './Pages/About';
 import FourOFour from './Pages/404';
-import {GithubProvider} from './context/github/GithubContext.js';
+import {GithubProvider} from './context/github/GithubContext';
+import {AlertProvider} from './context/alert/AlertContext';
 
 function App () {
   return (
     <GithubProvider>
-      <Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />;
+                <Route path="/fourofour" element={<FourOFour />} />;
+                <Route path="/*" element={<FourOFour />} />;
 
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />;
-              <Route path="/fourofour" element={<FourOFour />} />;
-              <Route path="/*" element={<FourOFour />} />;
+              </Routes>
 
-            </Routes>
-          </main>
+            </main>
 
-          <Footer />
-        </div>
+            <Footer />
+          </div>
 
-      </Router>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
