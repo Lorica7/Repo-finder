8edee0ea9fd1fa927de7b644605c50d1d5596b92
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-07-01 16:28:34 -0400

    user page working

--- repo-finder/src/Pages/User.jsx
+++ repo-finder/src/Pages/User.jsx
@@ -8,17 +8,21 @@ import {
   FaStore,
   FaUserFriends,
   FaUsers,
-} from 'react-fontawesome';
+} from 'react-icons/fa';
 
 import {Link} from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
 
 import GithubContext from '../context/github/GithubContext';
 
-export const User = () => {
+const User = () => {
   const {getUser, user, loading} = useContext (GithubContext);
 
   const params = useParams ();
+
+  useEffect (() => {
+    getUser (params.login);
+  }, []);
 
   const {
     name,
@@ -30,16 +34,12 @@ export const User = () => {
     twitter_username,
     login,
     html_url,
-    followers_url,
+    followers,
     following_url,
     public_repos,
     public_gists,
     hireable,
   } = user;
-
-  useEffect (() => {
-    getUser (params.login);
-  }, []);
 
   if (loading) {
     return <Spinner />;
@@ -70,6 +70,50 @@ export const User = () => {
               Visit Github Profile
             </a>
           </div>
+        </div>
+        <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+          {location &&
+            <div className="stat">
+              <div className="stat-title text-md"> Location </div>
+
+              <div className="text-lg stat-value"> {Location}</div>
+            </div>}
+          {blog &&
+            <div className="stat">
+              <div className="stat-title text-md"> Website </div>
+              <div className="text-lg stat-value">
+                <a href={`https://${blog}`} target="_blank" rel="noreferrer">
+                  {' '}{blog}{' '}
+                </a>
+                {blog}
+              </div>
+            </div>}
+          {twitter_username &&
+            <div className="stat">
+              <div className="stat-title text-md"> Twitter </div>
+              <div className="text-lg stat-value">
+                <a
+                  href={`https://twitter.com/${twitter_username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {' '}{twitter_username}{' '}
+                </a>
+
+              </div>
+            </div>}
+
+        </div>
+      </div>
+      <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl md:text-5xl" />{' '}
+          </div>
+          <div className="stat-title pr-5">
+            Followers
+          </div>
+          <div className="text-3xl md:text-4xl">{followers} </div>
         </div>
       </div>
     </div>
--- repo-finder/src/components/users/UserItem.jsx
+++ repo-finder/src/components/users/UserItem.jsx
@@ -5,17 +5,17 @@ import {Link} from 'react-router-dom';
 import GithubContext from '../../context/github/GithubContext';
 import PropTypes from 'prop-types';
 
-function UserItem({user: {login, avatar_url}}) {
+const UserItem = ({user: {login, avatar_url}}) => {
   return (
-    <div className="card shadow-md compact side bg-base-100">
-      <div className="felx-row items-center space-x-4 card-body">
+    <div className="card shadow-md  user-card side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
         <div className="avatar">
           <div className="rounded-full shadow w-14 h-14">
             <img src={avatar_url} alt="Profile" />
           </div>
           <h2 className="card-title">{login}</h2>
           <Link
-            className="text-base-content text-opacity-40"
+            className="text-base-content "
             to={`/user/:${login}`}
             // onClick={getUser (login)}
           >
@@ -25,7 +25,7 @@ function UserItem({user: {login, avatar_url}}) {
       </div>
     </div>
   );
-}
+};
 
 UserItem.propTypes = {
   user: PropTypes.object.isRequired,
